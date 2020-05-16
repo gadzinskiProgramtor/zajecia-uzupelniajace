2f7b84f14356a353f0be328e6e6adba195af837c
Gadzinski <mikigad@wp.pl>
2020-05-16 10:28:13 +0200

    Singleton #4 - singleton implementation with enum

--- src/main/java/pl/gadzinski/singleton/QueueSingletonEnum.java
+++ src/main/java/pl/gadzinski/singleton/QueueSingletonEnum.java
@@ -1,4 +1,25 @@
 package pl.gadzinski.singleton;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public enum QueueSingletonEnum {
-}
+    ;
+    private List<String> names;
+
+    QueueSingletonEnum(List<String> names) {
+        this.names = new LinkedList<>();
+    }
+
+    public void push(String name){
+        this.names.add(name);
+    }
+    public String pop(){
+        if(this.names.isEmpty()){
+            throw  new RuntimeException("Queue is empty");
+        }else {
+            String value = this.names.get(0);
+            this.names.remove(0);
+            return value;
+        }
+}}
